d74bee13c753a7ae5072b4fc0b55f632048189c5
YusufSuleman <63107868+YusufSuleman@users.noreply.github.com>
2021-08-02 20:16:16 +0500

    Version 2

--- cutelog.cpp
+++ cutelog.cpp
@@ -5,32 +5,59 @@ bool lightMode = false;
 
 void error(const char* message) {
 	if (lightMode == false) {
-		std::cout << "\u001b[41m\u001b[37;1mERROR:" << "\u001b[40m\u001b[0m " << message << "\n";
+		std::cout << "\u001b[41m\u001b[37;1mERROR  :" << "\u001b[40m\u001b[0m " << message << " \n";
 	}
 	else if (lightMode == true) {
-		std::cout << "\u001b[41m\u001b[37;1mERROR:" << "\u001b[47;1m\u001b[38;5;0m " << message << "\n";
+		std::cout << "\u001b[41m\u001b[37;1mERROR  :" << "\u001b[47;1m\u001b[38;5;0m " << message << " \n";
+	}
+}
+
+void errorMultiLine(const char* message) {
+	if (lightMode == false) {
+		std::cout << "\u001b[41m\u001b[37;1m        " << "\u001b[40m\u001b[0m " << message << " \n";
+	}
+	else if (lightMode == true) {
+		std::cout << "\u001b[41m\u001b[37;1m        " << "\u001b[47;1m\u001b[38;5;0m " << message << " \n";
+	}
+}
+
+void infoMultiline(const char* message)
+{
+	if (lightMode == false) {
+		std::cout << "\u001b[46;1m\u001b[37;1m        " << "\u001b[40m\u001b[0m " << message << " \n";
+	}
+	else if (lightMode == true) {
+		std::cout << "\u001b[46;1m\u001b[37;1m        " << "\u001b[47;1m\u001b[38;5;0m " << message << " \n";
 	}
 }
 
 void info(const char* message)
 {
 	if (lightMode == false) {
-		std::cout << "\u001b[46;1m\u001b[37;1mINFO:" << "\u001b[40m\u001b[0m " << message << "\n";
+		std::cout << "\u001b[46;1m\u001b[37;1mINFO   :" << "\u001b[40m\u001b[0m " << message << " \n";
 	}
 	else if (lightMode == true) {
-		std::cout << "\u001b[46;1m\u001b[37;1mINFO:" << "\u001b[47;1m\u001b[38;5;0m " << message << "\n";
+		std::cout << "\u001b[46;1m\u001b[37;1mINFO   :" << "\u001b[47;1m\u001b[38;5;0m " << message << " \n";
 	}
 }
 
 void warning(const char* message) {
 	if (lightMode == false) {
-		std::cout << "\u001b[43;1m\u001b[37;1mWARNING:" << "\u001b[40m\u001b[0m " << message << "\n";
+		std::cout << "\u001b[43;1m\u001b[37;1mWARNING:" << "\u001b[40m\u001b[0m " << message << " \n";
 	}
 	else if (lightMode == true) {
-		std::cout << "\u001b[43;1m\u001b[37;1mWARNING:" << "\u001b[47;1m\u001b[38;5;0m " << message << "\n";
+		std::cout << "\u001b[43;1m\u001b[37;1mWARNING:" << "\u001b[47;1m\u001b[38;5;0m " << message << " \n";
 	}
 }
 
+void warningMultline(const char* message) {
+	if (lightMode == false) {
+		std::cout << "\u001b[43;1m\u001b[37;1m        " << "\u001b[40m\u001b[0m " << message << " \n";
+	}
+	else if (lightMode == true) {
+		std::cout << "\u001b[43;1m\u001b[37;1m        " << "\u001b[47;1m\u001b[38;5;0m " << message << " \n";
+	}
+}
 void toggleToLightMode() {
 	lightMode = true;
 }
@@ -49,12 +76,22 @@ void cuteLogExample() {
 	warning("Light");;
 	toggleToDarkMode();
 	error("Dark");
+	errorMultiLine("Dark");
 	info("Dark");
+	infoMultiline("Dark");
 	warning("Dark");
+	warningMultline("Dark");
+	toggleToLightMode();
+	error("Light");
+	errorMultiLine("Light");
+	info("Light");
+	infoMultiline("Light");
+	warning("Light");
+	warningMultline("Light");
 }
 
 
 int main() 
 {
-	
+	cuteLogExample();
 }
--- cutelog.h
+++ cutelog.h
@@ -4,8 +4,9 @@
 bool lightMode = false;
 
 void error(const char* message);
+void errorMultiLine(const char* message);
 void info(const char* message);
+void infoMultiline(const char* message);
 void warning(const char* message);
+void warningMultline(const char* message);
 void cuteLogExample();
-void toggleToLightMode();
-void toggleToDarkMode();
